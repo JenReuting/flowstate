532186b715d44f51c9d201af8d12c357013dc3f3
Jennifer Reuting <jenreuting@yahoo.com>
2023-02-01 17:25:10 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+####################################################################
---
+++ templates/models.py
@@ -0,0 +1,5 @@
+""" SQLAlchemy models for FlowState """
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
