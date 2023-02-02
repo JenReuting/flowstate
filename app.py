8b9ed14a0d09e879c7bab1902172a197d7a026ff
Jennifer Reuting <jenreuting@yahoo.com>
2023-02-01 17:36:59 -0800

    created project structure, set up env variables and turned off caching

--- app.py
+++ app.py
@@ -12,10 +12,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
+# toolbar = DebugToolbarExtension(app)
+
 ####################################################################
+
+
+
+
+
+
+
+
+##############################################################################
+# Turn off all caching in development
+
+@app.after_request
+def add_header(response):
+    """Add non-caching headers on every response."""
+    response.cache_control.no_cache = True
+    return response
--- models.py
+++ models.py
@@ -3,3 +3,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def connect_db(app):
+    """ Connect to database. """
+    db.app = app
+    db.init_app(app)
